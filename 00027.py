a85d6b06fa8a332ba774e2749f907ad8a7e598c9
HakurrrPUNK <82894964+farzanekram07@users.noreply.github.com>
2025-05-23 22:44:09 +0530

    Update 00027.py

--- 00027.py
+++ 00027.py
@@ -1,5 +1,23 @@
-class Solution:
-    def remove_element(nums, val):
+# brute force
+def remove_element0(nums, val):
+    result = []
+    for i in nums:
+        if i != val:
+            result.append(i)
+    return result
+
+# optimization - using two pointer
+def remove_element1(nums, val):
+    start = 0
+    for i, num in enumerate(nums):
+        if num == val:
+            continue
+        else:
+            nums[start] = nums[i]
+            start += 1
+    return nums[:start]
+
+def remove_element2(nums, val):
     index = 0
     for i in range(len(nums)):
         if nums[i] != val:
