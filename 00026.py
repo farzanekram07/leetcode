6eb15d510a4afa1c763da5fbdcbdb59d38b33f61
HakurrrPUNK <82894964+farzanekram07@users.noreply.github.com>
2025-05-24 18:21:26 +0530

    Update 00026.py

--- 00026.py
+++ 00026.py
@@ -1,8 +1,23 @@
-class Solution(object):
-        def removeDuplicates(self, nums):
-            unique_count = 1
-            for i in range(1, len(nums)):
-                if nums[i-1] != nums[i]:
-                    nums[unique_count] = nums[i]
-                    unique_count += 1
-            return unique_count
+# brute force - O(n2)|O(n)
+def remove_duplicates(nums):
+    result = []
+    for i in nums:
+        if i not in result:
+            result.append(i)
+    return result 
+
+# hashmap - O(n)|O(n)
+def remove_duplicates1(nums):
+    result = set()
+    for i in nums:
+        result.add(i)
+    return list(result)
+
+# two-pointer -> in-place - O(n)|O(1) 
+def remove_duplicates2(nums):
+    i = 0
+    for j in range(1, len(nums)):
+        if nums[j] != nums[i]:
+            i += 1
+            nums[i] = nums[j]
+    return nums[:i+1]
